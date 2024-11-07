358bd620c19ce473f563b2491614ea71490c4cb0
Devin Atkin <devinatkin@gmail.com>
2024-11-06 18:57:56 -0700

    Initial Commit

---
+++ GDS-Examiner/GDS_ExaminerApp.swift
@@ -0,0 +1,18 @@
+//
+//  GDS_ExaminerApp.swift
+//  GDS-Examiner
+//
+//  Created by Devin Michael Atkin on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct GDS_ExaminerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ GDS-ExaminerTests/GDS_ExaminerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GDS_ExaminerTests.swift
+//  GDS-ExaminerTests
+//
+//  Created by Devin Michael Atkin on 2024-11-06.
+//
+
+import Testing
+@testable import GDS_Examiner
+
+struct GDS_ExaminerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
